9019d37d407477c9f21076a60ce1b83757a6205c
sobakacece <sobakacece@skiff.com>
2024-09-01 19:45:28 +0300

    Added snapping

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,11 @@ extends CharacterBody3D
 class_name Player
 
 @export_category("Movement")
-@export var jump_velocity = 4.5
+@export var jump_velocity = 17
 @export var fall_acceleration = 75
+@export var jump_state_time = 0.2
 @export var turn_speed : float = 1
 @export var turn_curve : Curve
-@export var mass = 10
 
 @export_category("Acceleration")
 @export var acceleration_curve : Curve
@@ -30,18 +30,23 @@ var direction
 @export_category("Camera Rotation")
 @export var rotation_weight = 0.1;
 @export var rotation_speed_mod : float = 1
+@export var camera_range_y_axis : Vector2 = Vector2(-90, 90)
 var target_camera_rotation: Vector3
 var smooth_rotation: Vector3
 
 #should be hidden later
 @export_category("General")
 @export var mouse_sensitivity = 0.01
+@export var snapping_distance = 2
+@export var mass = 3
 
 var slope_const = 2
 #nodes
 var spring_arm
 var visuals
 var dash_timer : SceneTreeTimer
+var jump_timer : SceneTreeTimer
+var is_jumping : bool = false
 
 #dynamic_values
 var current_acceleration_boost
@@ -61,7 +66,7 @@ func _input(event):
 		target_camera_rotation.y -= event.relative.x * mouse_sensitivity * rotation_speed_mod
 		#target_rotation.y = wrapf(target_rotation.y, 0, 360)
 		target_camera_rotation.x -= event.relative.y * mouse_sensitivity * rotation_speed_mod
-		target_camera_rotation.x = clamp(target_camera_rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		target_camera_rotation.x = clamp(target_camera_rotation.x, deg_to_rad(camera_range_y_axis.x), deg_to_rad(camera_range_y_axis.y))
 		#print(target_rotation.x);
 		
 
@@ -70,6 +75,10 @@ func _process(delta: float) -> void:
 	smooth_rotation = smooth_rotation.lerp(target_camera_rotation, delta * rotation_weight)
 	spring_arm.rotation = smooth_rotation
 	spring_arm.position = position
+	#camera_range_y_axis.y=clamp(camera_range_y_axis.y + Vector3.FORWARD.angle_to(get_normal()), -90, -10)
+	#print(spring_arm.spring_length)
+	#if ($SpringArm/Camera3D.position.distance_to(Vector3.ZERO) < 2):
+		#print($SpringArm/Camera3D.position)
 
 func slope_process(delta : float):
 	return Vector3(get_normal().normalized().x, 0, get_normal().normalized().z) * delta * slope_const
@@ -77,7 +86,7 @@ func slope_process(delta : float):
 	
 func get_normal():
 	if $RayCast3D.is_colliding():
-		return $RayCast3D.get_collision_normal() 
+		return $RayCast3D.get_collision_normal()
 	else:
 		return Vector3.UP
 
@@ -85,8 +94,6 @@ func get_normal():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
-	if not is_on_floor():
-		velocity += get_gravity() * delta * mass
 		#print("NOT ON FLOOR")
 	#else:
 		#var normal = get_normal().normalized()
@@ -97,10 +104,23 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
+		is_jumping = true
+		if !jump_timer:
+			jump_timer = get_tree().create_timer(jump_state_time, false, true)
+			jump_timer.timeout.connect(func(): jump_end_handle())
 	
 	update_velocity_input(delta)
-	
+	#print(is_jumping)
+	if not is_on_floor():
+		print($Slop_end_check.is_colliding())
+		if ($RayCast3D.get_collision_point().distance_to(position) < snapping_distance && $Slop_end_check.is_colliding() && !is_jumping):
+			position = $RayCast3D.get_collision_point()
+			#is_jumping = false
+			#print("SNAPPED")
+		
+		velocity += get_gravity() * delta * mass
 	move_and_slide()
+	#$SpringArm/Camera3D.position = clamp($SpringArm/Camera3D.position, Vector3(-1, -1, 4), Vector3(5,5,5))
 	
 	if velocity.length() > 0.2:
 		rotate_visuals(delta)
@@ -136,15 +156,17 @@ func rotate_visuals(delta : float):
 
 	var target_rotation = Vector3(current_rot.x, atan2(-velocity.x, -velocity.z), current_rot.z)
 	var target_quat = Quaternion(Basis.from_euler(target_rotation))
-
+#	print(camera_range_y_axis)
 	# Slerp between current and target Quaternions
 	var smooth_rot_quat = current_quat.slerp(target_quat, turn_speed * delta)
-
 	# Convert back to Euler for visuals.rotation
 	visuals.rotation = smooth_rot_quat.get_euler()
 func on_stop():
 	return
 	#print("stopped")
+func jump_end_handle():
+	jump_timer = null
+	is_jumping = false
 
 func get_dash_boost() -> float:
 	if Input.is_action_just_pressed("dash") && !dash_timer:
